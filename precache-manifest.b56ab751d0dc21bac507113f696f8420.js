7ccae006aa15ea1713a913fd7929e3534c2f83ef
allcantara <alcantara.desenvolvedor@gmail.com>
2020-05-14 16:05:11 -0300

    Updates

---
+++ precache-manifest.b56ab751d0dc21bac507113f696f8420.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e79c8b3808f4798d1b9ee7f9de19bd02",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ce8ee797a347dacee7e7",
+    "url": "/static/css/main.f632366a.chunk.css"
+  },
+  {
+    "revision": "705880d61e591b931055",
+    "url": "/static/js/2.669d5111.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.669d5111.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ce8ee797a347dacee7e7",
+    "url": "/static/js/main.ee89ed89.chunk.js"
+  },
+  {
+    "revision": "227f05271dc90b39cfd8",
+    "url": "/static/js/runtime-main.acea2040.js"
+  }
+]);
