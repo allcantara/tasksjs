94cfc2e45f017f9cccbf0c8189edb0c667fff44f
allcantara <alcantara.desenvolvedor@gmail.com>
2020-05-15 10:52:11 -0300

    Updates

--- precache-manifest.765788b62c7f28619f310b3fb05ccda8.js
+++ precache-manifest.765788b62c7f28619f310b3fb05ccda8.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "5b50c877753ea17b19ab7bd7fc00f4eb",
+    "revision": "9a3584df23a608117e7bffdb879586fb",
     "url": "/index.html"
   },
   {
-    "revision": "02f5c48eb6a088432f53",
+    "revision": "392e262e6f354ef921e0",
     "url": "/static/css/main.f632366a.chunk.css"
   },
   {
-    "revision": "705880d61e591b931055",
-    "url": "/static/js/2.669d5111.chunk.js"
+    "revision": "12988b0a876bd0716921",
+    "url": "/static/js/2.e074eab6.chunk.js"
   },
   {
     "revision": "c64c486544348f10a6d6c716950bc223",
-    "url": "/static/js/2.669d5111.chunk.js.LICENSE.txt"
+    "url": "/static/js/2.e074eab6.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "02f5c48eb6a088432f53",
-    "url": "/static/js/main.d27e15e6.chunk.js"
+    "revision": "392e262e6f354ef921e0",
+    "url": "/static/js/main.bf314d51.chunk.js"
   },
   {
     "revision": "227f05271dc90b39cfd8",
