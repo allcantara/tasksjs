553d9f326b541ac446030890a65b6b582120616d
allcantara <alcantara.desenvolvedor@gmail.com>
2020-05-15 00:55:51 -0300

    Mudança da url e novo build

---
+++ build/precache-manifest.e745dc68c4dc456c2cefebc0e33c32c1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5b50c877753ea17b19ab7bd7fc00f4eb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "02f5c48eb6a088432f53",
+    "url": "/static/css/main.f632366a.chunk.css"
+  },
+  {
+    "revision": "705880d61e591b931055",
+    "url": "/static/js/2.669d5111.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.669d5111.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "02f5c48eb6a088432f53",
+    "url": "/static/js/main.d27e15e6.chunk.js"
+  },
+  {
+    "revision": "227f05271dc90b39cfd8",
+    "url": "/static/js/runtime-main.acea2040.js"
+  }
+]);
